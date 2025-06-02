bcd84093e9f958aea8661d489349a33be098829e
juuxi <dimashopyrev@mail.ru>
2025-06-02 20:37:52 +0300

    manufacturer col added to teams

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ with open('team-driver.csv', newline='') as f:
     for row in reader:
         teams.append(row[1])
 
+with open('team-manufacturer.csv') as f:
+    reader = csv.reader(f)
+    manufacturer_team = dict(reader)
+
 conn = psycopg2.connect(database="nascar", user="juuxi", password="111", host="localhost", port=5432)
 
 cursor = conn.cursor()
@@ -48,7 +52,8 @@ cursor.execute("""
     CREATE TABLE IF NOT EXISTS teams (
     place SERIAL PRIMARY KEY,
     name  VARCHAR(40),
-    points INTEGER               
+    points INTEGER,  
+    manufacturer VARCHAR(15)             
 )
 """)
 
@@ -58,6 +63,30 @@ cursor.execute("""
     GROUP BY team
     ORDER BY points DESC
 """)
+
+chevrolet_teams = manufacturer_team["Chevrolet"].split(sep="\n")
+ford_teams = manufacturer_team["Ford"].split(sep="\n")
+toyota_teams = manufacturer_team["Toyota"].split(sep="\n")
+
+
+for manufacturer, teams in manufacturer_team.items():
+    teams = teams.split('\n')
+
+    placeholders = ', '.join(['%s'] * len(teams))
+    query = f"""
+        UPDATE teams
+        SET manufacturer = %s
+        WHERE name IN ({placeholders})
+    """
+
+    cursor.execute(query, [manufacturer] + teams)
+
+cursor.execute("""
+    SELECT * FROM teams
+    ORDER BY points DESC
+""")
+
+print(cursor.fetchall())
 
 # Сохраняем изменения и закрываем соединение
 conn.commit()
